2082a0a0e867bd2f3840d1f5bc3ab81795a34dbf
Hamza Nouinou <nouinouhamza@gmail.com>
2023-03-14 01:11:37 +0100

    chore: cleanup + lint + prettier

---
+++ src/pages/index.jsx
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>NexJS Image Gallery App</title>
+        <meta name="description" content="This app was built by Hamza Nouinou" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div>content goes here</div>
+    </>
+  );
+}
